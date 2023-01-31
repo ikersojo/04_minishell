9e8d624242cb25ddb63c1e220bab31998c2de04a
Iker Sojo <ikersojo@hotmail.com>
2023-01-31 22:24:31 +0100

    added further flags, parenthesis levels and debug output to parsing

--- src/04_parse_cmds.c
+++ src/04_parse_cmds.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 22:45:07 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/01/30 22:16:37 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/01/31 22:21:45 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,34 +23,57 @@ static t_cmd	*ft_cmdnew(char *str, int index)
 	new->index = index;
 	new->is_outfd = 0;
 	new->is_infd = 0;
+	new->is_par = 0;
+	new->par_lvl = 0;
+	new->is_and = 0;
+	new->is_or = 0;
 	if (*str == '>')
 		new->is_outfd = 1;
 	if (*str == '<')
 		new->is_infd = 1;
+	if (*str == '(' || *str == ')')
+		new->is_par = 1;
+	if (*str == '&' && *(str + 1) == '&')
+		new->is_and = 1;
+	if (*str == '|' && *(str + 1) == '|')
+		new->is_or = 1;
 	new->next = NULL;
 	return (new);
 }
 
-static t_cmd	*ft_cmdlast(t_cmd *cmd)
+static t_cmd	*ft_cmdlast(t_cmd *cmd, int *par_lvl)
 {
 	if (cmd == NULL)
 		return (NULL);
 	while (cmd->next)
+	{
+		if (*cmd->str == '(')
+			*par_lvl += 1;
+		else if (*cmd->str == ')')
+			*par_lvl -= 1;
 		cmd = cmd->next;
+	}
+	if (*cmd->str == '(')
+		*par_lvl += 1;
+	else if (*cmd->str == ')')
+		*par_lvl -= 1;
 	return (cmd);
 }
 
 static void	ft_cmdadd_back(t_cmd **cmd, t_cmd *new)
 {
 	t_cmd	*tmp;
+	int		par_lvl;
 
+	par_lvl = 0;
 	if (new && cmd)
 	{
 		if (*cmd == NULL)
 			*cmd = new;
 		else
 		{
-			tmp = ft_cmdlast(*cmd);
+			tmp = ft_cmdlast(*cmd, &par_lvl);
+			new->par_lvl = par_lvl;
 			tmp->next = new;
 		}
 	}
@@ -71,6 +94,11 @@ void	ft_parse(t_data *data)
 		{
 			str = ft_substr(data->ex_input, i, ft_endredir(data->ex_input, i));
 			i += ft_endredir(data->ex_input, i);
+		}
+		else if (ft_ischarset(*(data->ex_input + i), "()"))
+		{
+			str = ft_substr(data->ex_input, i, 1);
+			i++;
 		}
 		else
 		{
--- src/99_debug.c
+++ src/99_debug.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/30 20:46:36 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/01/30 23:12:49 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/01/31 22:03:37 by isojo-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,9 @@ void	ft_show_parsed(t_data *data)
 	while (temp)
 	{
 		printf("    parsed cmd %d: %s\n", temp->index, temp->str);
+		printf("    infd %d outfd %d par %d (%d) and %d or %d\n",
+			temp->is_infd, temp->is_outfd, temp->is_par, temp->par_lvl,
+			temp->is_and, temp->is_or);
 		temp = temp->next;
 	}
 	printf("\n    ----------------\033[0;39m\n\n");
