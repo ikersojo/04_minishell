462af8cfbc749c8002ce05c702d9ecaade119bae
tinitiuset <tinitiuset@gmail.com>
2023-03-05 11:01:50 +0100

    Updated TODO List.

--- src/00_main.c
+++ src/00_main.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/16 22:22:47 by isojo-go          #+#    #+#             */
-/*   Updated: 2023/03/04 19:59:20 by mvalient         ###   ########.fr       */
+/*   Updated: 2023/03/05 10:14:23 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,13 +62,11 @@ int	main(int argc, char **argv, char **envp)
 		prompt();
 }
 
-// TODO : Si se borra el path del env hay segfault
-// TODO : Revisar leaks
-// TODO : Revisar prompt extra al hacer ctrl + C sobre un proceso en segundo plano
-// TODO : Revisar export con ""=""
-// TODO : Revisar export en general
-// TODO : export y env de bash me printan 2 líneas de más con wc -l
-// TODO : No sé por qué se printa el input de readline
+// TODO : Si se borra el path del env hay segfault -> Comprobar si existe path antes de hacer execve (Iker)
+// TODO : Revisar caso de comillas sin contenido (Iker)
+// TODO : export y env de bash me printan 2 líneas de más con wc -l (Martin)
+// TODO : No sé por qué se printa el input de readline -> revisar al final
+// TODO : Revisar leaks (Martin)
 
 // Último update (iker 28-02-2023):
 	// fixed merge
