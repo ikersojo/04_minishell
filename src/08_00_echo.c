afa2e7b6b62d4619d4c2a05e28c4c294d86889e7
tinitiuset <tinitiuset@gmail.com>
2023-03-06 22:33:00 +0100

    BUGFIX: Echo just evaluates first argument. Now doesn't.

--- src/08_00_echo.c
+++ src/08_00_echo.c
@@ -6,7 +6,7 @@
 /*   By: isojo-go <isojo-go@student.42urduliz.co    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 22:11:56 by mvalient          #+#    #+#             */
-/*   Updated: 2023/02/28 23:26:00 by isojo-go         ###   ########.fr       */
+/*   Updated: 2023/03/06 22:31:44 by mvalient         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,11 +24,8 @@ int	echo_builtin(t_vars **env, char **cmd)
 	newline = 1;
 	if (ft_strcmp(cmd[i], "echo"))
 		return (printf("RTFM: Undefined error.\n"));
-	if (!ft_strcmp(cmd[++i], "-n"))
-	{
+	while (cmd [++i] && !ft_strcmp(cmd[i], "-n"))
 		newline = 0;
-		i++;
-	}
 	while (cmd[i])
 	{
 		printf("%s", cmd[i++]);
